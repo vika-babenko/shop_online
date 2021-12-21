8c71fa118fc71b54b02dcf2a591596f480c4b35f
vika--babenko <vika.babenko.18.09.02@gmail.com>
2021-12-21 21:37:09 +0200

    initial

---
+++ src/components/cart/CartSummaryPaymentCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cart-item-card">
+    <h1>Cart total: ${{cart_total.toFixed(2)}}</h1>
+    <button class="view-product-button">Pay by Card</button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed:{
+    cart_total() {
+      return this.$store.getters.cartTotal
+    }
+  }
+}
+</script>
---
+++ src/data/items.js
@@ -0,0 +1,46 @@
+const items = [
+    {
+        id:1,
+        name: 'Comfy Slippers',
+        price: 12.00,
+        category: 'Footwear',
+        description: 'Investing in a pair of cozy slippers is always a good idea. The best women\'s slippers are one' +
+            ' of the most comfortable things you can slide on, and usually signal a relaxing transition in your day'
+    },
+    {
+        id:2,
+        name: 'Office blouse',
+        price: 40.50,
+        category: 'Office style',
+        description: "Blouse - women's light clothing made of thin fabric in the form of a short fitted shirt."+
+            "The blouse contains sleeves, collar and cuffs. It is often fastened with buttons, but there are also" +
+            " tunic-like blouses. Common jewelry in the form of frogs, frills, ruffles, as well as applications of beads."
+    },
+    {
+        id:3,
+        name: 'Pajamas',
+        price: 30.50,
+        category: 'Sleepwear',
+        description: "pajamas are soft, warm, and traditionally loose garments derived from the Indian and Persian" +
+            " bottom-wear, the pyjamas."
+    },
+    {
+        id:4,
+        name: 'T-Shirt',
+        price: 10.50,
+        category: 'Casual wear',
+        description: "a style of fabric shirt named after the T shape of its body and sleeves. Traditionally, it has" +
+            " short sleeves and a round neckline, known as a crew neck, which lacks a collar. T-shirts are generally" +
+            " made of a stretchy, light, and inexpensive fabric and are easy to clean."
+    },
+    {
+        id:5,
+        name: 'Hair bands',
+        price: 1.50,
+        category: 'For hair',
+        description: "Check out our hair bands selection for the very best in unique or custom," +
+            " handmade pieces from our headbands & turbans shops."
+    }
+]
+
+export default items
